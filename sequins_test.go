4d0648ad3abd980c01c317de3f55d33d49546a04
Colin Marc <colinmarc@gmail.com>
2015-03-03 19:14:50 +0100

    missing import

--- sequins_test.go
+++ sequins_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/stripe/sequins/backend"
 	"net/http"
 	"net/http/httptest"

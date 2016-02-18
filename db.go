7aaeb2af1eeb8bec6c433b00bb0e40c0df05c666
Colin Marc <colinmarc@gmail.com>
2016-02-18 12:51:04 +0100

    Forgot to commit two important files

---
+++ db.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+)
+
+type db struct {
+	sequins *sequins
+
+	name        string
+	mux         *versionMux
+	refreshLock sync.Mutex
+	status      dbStatus
+}
+
+type dbStatus struct {
+	CurrentVersion versionStatus            `json:"current_version"`
+	Versions       map[string]versionStatus `json:"versions"`
+}
+
+type versionStatus struct {
+	Path    string `json:"path"`
+	Created int64  `json:"created"`
+	State   string `json:"state"`
+}
+
+func newDB(sequins *sequins, name string) *db {
+	return &db{
+		sequins: sequins,
+		name:    name,
+		mux:     newVersionMux(),
+	}
+}
+
+func (db *db) refresh() error {
+	db.refreshLock.Lock()
+	defer db.refreshLock.Unlock()
+
+	versions, err := db.sequins.backend.ListVersions(db.name, db.sequins.config.RequireSuccessFile)
+	if err != nil {
+		return err
+	} else if len(versions) == 0 {
+		log.Println("No versions available for", db.name)
+	}
+
+	latestVersion := versions[len(versions)-1]
+	currentVersion := db.mux.getCurrent()
+	db.mux.release(currentVersion)
+	if currentVersion != nil && latestVersion == currentVersion.name {
+		// TODO: we log this a bunch uneccessarily.
+		log.Println(latestVersion, "is already the newest version of", db.name, "so not reloading.")
+		return nil
+	}
+
+	files, err := db.sequins.backend.ListFiles(db.name, latestVersion)
+	if err != nil {
+		return err
+	}
+
+	builder := newVersion(db.sequins, db.name, latestVersion, len(files))
+	vs, err := builder.build()
+	if err != nil {
+		return err
+	}
+
+	// Prepare the version, so that during the switching period we can respond
+	// to requests for it.
+	db.mux.prepare(vs)
+
+	// Then, wait for all our peers to be ready. All peers should all see that
+	// everything is ready at roughly the same time. If they switch before us,
+	// that's fine; we can serve the new version we've 'prepared' when peers
+	// ask for it. If we're not part of a cluster, this returns immediately.
+	vs.waitForPeers()
+
+	// Now we can start serving requests for the new version to clients, as well.
+	log.Printf("Switching to version %s of %s!", vs.name, db.name)
+	db.mux.upgrade(vs)
+
+	// Finally, clean up the old version after some amount of time and after
+	// no peers are asking for it anymore. Again, this is immediate if there are
+	// no peers to consider.
+	if currentVersion != nil {
+		go func() {
+			shouldWait := (db.sequins.peers != nil)
+			db.mux.remove(currentVersion, shouldWait)
+			log.Println("Removing and clearing version", currentVersion.name, "of", db.name)
+
+			// TODO: this doesn't actually delete the data
+			currentVersion.close()
+
+			// Grab the lock again so we can re-cache the status JSON.
+		}()
+	}
+
+	db.cacheStatus()
+	return nil
+}
+
+func (db *db) cacheStatus() {
+
+}
+
+func (db *db) serveKey(w http.ResponseWriter, r *http.Request, key string) {
+	if key == "" {
+		jsonBytes, err := json.Marshal(db.status)
+		if err != nil {
+			log.Println("Error serving status:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Header()["Content-Type"] = []string{"application/json"}
+		w.Write(jsonBytes)
+		return
+	}
+
+	db.mux.serveKey(w, r, key)
+}
+
+func (db *db) close() error {
+	db.refreshLock.Lock()
+	defer db.refreshLock.Unlock()
+
+	var err error
+	for _, vs := range db.mux.getAll() {
+		closeErr := vs.close()
+		if closeErr != nil {
+			err = closeErr
+		}
+	}
+
+	return err
+}
---
+++ partitions.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"path"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+// partitions represents a list of partitions for a single version and their
+// mapping to nodes, synced from zookeeper. It's also responsible for
+// advertising the partitions we have locally.
+type partitions struct {
+	peers     *peers
+	zkWatcher *zkWatcher
+
+	db      string
+	version string
+	zkPath  string
+
+	total   int
+	missing int
+	local   map[int]bool
+	remote  map[int][]string
+
+	lock  sync.RWMutex
+	ready chan bool
+}
+
+func watchPartitions(zkWatcher *zkWatcher, peers *peers, db, version string, numPartitions, replication int) *partitions {
+	p := &partitions{
+		peers:     peers,
+		zkWatcher: zkWatcher,
+		db:        db,
+		version:   version,
+		zkPath:    path.Join("partitions", db, version),
+		total:     numPartitions,
+		remote:    make(map[int][]string),
+		ready:     make(chan bool),
+	}
+
+	p.pickLocalPartitions(replication)
+
+	// Create the partitions path we're going to watch, in case no one has done
+	// that yet.
+	p.zkPath = path.Join("partitions", db, version)
+	zkWatcher.createPath(p.zkPath)
+
+	updates := zkWatcher.watchChildren(p.zkPath)
+	go p.sync(updates)
+	return p
+}
+
+// pickLocalPartitions selects which partitions are local by iterating through
+// them all, and checking the hashring to see if this peer is one of the
+// replicas.
+func (p *partitions) pickLocalPartitions(replication int) {
+	local := make(map[int]bool)
+	disp := make([]int, 0)
+
+	for i := 0; i < p.total; i++ {
+		partitionId := p.partitionId(i)
+
+		replicas := p.peers.pick(partitionId, replication)
+		for _, replica := range replicas {
+			if replica == peerSelf {
+				local[i] = true
+				disp = append(disp, i)
+			}
+		}
+	}
+
+	log.Printf("Selected partitions for %s version %s: %v", p.db, p.version, disp)
+	p.local = local
+	p.missing = p.total - len(local)
+}
+
+// sync syncs the remote partitions from zoolander whenever they change.
+func (p *partitions) sync(updates chan []string) {
+	for {
+		nodes := <-updates
+		p.updatePartitions(nodes)
+	}
+}
+
+func (p *partitions) updatePartitions(nodes []string) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	remote := make(map[int][]string)
+	for _, node := range nodes {
+		parts := strings.SplitN(node, "@", 2)
+		partition, _ := strconv.Atoi(parts[0])
+		host := parts[1]
+		if host != p.peers.address {
+			remote[partition] = append(remote[partition], host)
+		}
+	}
+
+	// Check for each partition. If every one is available on at least one node,
+	// then we're ready to rumble.
+	p.remote = remote
+	missing := 0
+	for i := 0; i < p.total; i++ {
+		if _, ok := p.local[i]; ok {
+			continue
+		}
+
+		if _, ok := p.remote[i]; ok {
+			continue
+		}
+
+		missing += 1
+	}
+
+	p.missing = missing
+	if missing == 0 {
+		select {
+		case p.ready <- true:
+		default:
+		}
+	}
+}
+
+// advertiseAndWait advertises the partitions we have locally, and waits until
+// it sees at least one peer for every remote partition.
+func (p *partitions) advertiseAndWait() {
+	// Advertise that our local partitions are ready.
+	p.advertisePartitions()
+
+	for {
+		p.lock.RLock()
+		ready := (p.missing == 0)
+		p.lock.RUnlock()
+		if ready {
+			break
+		}
+
+		log.Printf("Waiting for all partitions of %s version %s to be available (missing %d)",
+			p.db, p.version, p.missing)
+
+		t := time.NewTimer(10 * time.Second)
+		select {
+		case <-t.C:
+		case <-p.ready:
+		}
+	}
+}
+
+// advertisePartitions creates an ephemeral node for each partition this local
+// peer is responsible for.
+// TODO: this should maybe be a zk multi op?
+func (p *partitions) advertisePartitions() {
+	for partition := range p.local {
+		node := fmt.Sprintf("%05d@%s", partition, p.peers.address)
+		p.zkWatcher.createEphemeral(path.Join(p.zkPath, node))
+	}
+}
+
+// getPeers returns the list of peers who have the given partition available.
+func (p *partitions) getPeers(partition int) []string {
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+
+	peers := make([]string, len(p.remote[partition]))
+	copy(peers, p.remote[partition])
+	return peers
+}
+
+// partitionId returns a string id for the given partition, to be used for the
+// consistent hashing ring. It's not really meant to be unique, but it should be
+// different for different versions with the same number of partitions, so that
+// they don't shard identically.
+func (p *partitions) partitionId(partition int) string {
+	return fmt.Sprintf("%s:%05d", p.zkPath, partition)
+}

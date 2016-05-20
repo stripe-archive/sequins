a0e53b02dadb9807ae5c9b7a53eb4206f0f42558
Jim Scott <scottjab@stripe.com>
2016-05-20 13:19:53 -0700

    Add a response header 'X-Sequins-Proxied-To'
    Exposes which server handled the request to the client.

--- proxy_test.go
+++ proxy_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -30,6 +31,7 @@ func httptestHost(s *httptest.Server) string {
 }
 
 func TestProxySinglePeer(t *testing.T) {
+	w := httptest.NewRecorder()
 	peer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "all good")
 	}))
@@ -37,12 +39,16 @@ func TestProxySinglePeer(t *testing.T) {
 	peers := []string{httptestHost(peer)}
 
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
+	// Port changes on every test run.
+	host, _, err := net.SplitHostPort(w.Header().Get("X-Sequins-Proxied-to"))
+	assert.Equal(t, host, "127.0.0.1")
 	assert.NoError(t, err, "simple proxying should work")
 	assert.NotNil(t, res, "simple proxying should work")
 }
 
 func TestProxySlowPeer(t *testing.T) {
+	w := httptest.NewRecorder()
 	slowPeer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Fprintln(w, "sorry, did you need something?")
@@ -58,7 +64,7 @@ func TestProxySlowPeer(t *testing.T) {
 
 	peers := []string{httptestHost(slowPeer), httptestHost(goodPeer), httptestHost(notReachedPeer)}
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
 	require.NoError(t, err, "proxying should work on the second peer")
 	require.NotNil(t, res, "proxying should work on the second peer")
 
@@ -66,6 +72,7 @@ func TestProxySlowPeer(t *testing.T) {
 }
 
 func TestProxyErrorPeer(t *testing.T) {
+	w := httptest.NewRecorder()
 	errorPeer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 	}))
@@ -80,7 +87,7 @@ func TestProxyErrorPeer(t *testing.T) {
 
 	peers := []string{httptestHost(errorPeer), httptestHost(goodPeer), httptestHost(notReachedPeer)}
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
 	require.NoError(t, err, "proxying should work on the second peer")
 	require.NotNil(t, res, "proxying should work on the second peer")
 
@@ -88,6 +95,7 @@ func TestProxyErrorPeer(t *testing.T) {
 }
 
 func TestProxySlowPeerErrorPeer(t *testing.T) {
+	w := httptest.NewRecorder()
 	slowPeer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(15 * time.Millisecond)
 		fmt.Fprintln(w, "all good, sorry to keep you waiting")
@@ -99,7 +107,7 @@ func TestProxySlowPeerErrorPeer(t *testing.T) {
 
 	peers := []string{httptestHost(slowPeer), httptestHost(errorPeer)}
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
 	require.NoError(t, err, "proxying should work on the first peer")
 	require.NotNil(t, res, "proxying should work on the first peer")
 
@@ -107,6 +115,7 @@ func TestProxySlowPeerErrorPeer(t *testing.T) {
 }
 
 func TestProxyTimeout(t *testing.T) {
+	w := httptest.NewRecorder()
 	slowPeer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Fprintln(w, "sorry, did you need something?")
@@ -124,12 +133,13 @@ func TestProxyTimeout(t *testing.T) {
 	}
 
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
 	assert.Equal(t, errProxyTimeout, err, "proxying should time out with all slow peers")
 	assert.Nil(t, res, "proxying should time out with all slow peers")
 }
 
 func TestProxyErrors(t *testing.T) {
+	w := httptest.NewRecorder()
 	errorPeer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(500)
 	}))
@@ -141,7 +151,7 @@ func TestProxyErrors(t *testing.T) {
 	}
 
 	r, _ := http.NewRequest("GET", "http://localhost", nil)
-	res, err := proxyTestVersion.proxy(r, peers)
+	res, err := proxyTestVersion.proxy(w, r, peers)
 	assert.Equal(t, errNoAvailablePeers, err, "proxying should return errNoAvailablePeers if all error")
 	assert.Nil(t, res, "proxying should return errNoAvailablePeers if all error")
 }

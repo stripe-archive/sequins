c391e135a24f19b66a28153d9c75c2f7e866b12a
Colin Marc <colinmarc@gmail.com>
2016-10-05 19:52:50 +0200

    Revert to using timer-based cancellation for proxied requests
    In c6eb0b, we switched to using context to manage proxying requests to
    peers and the timeouts therein.
    
    Using context still makes this flow easier, but using a context with a
    deadline had the unintended side effect that it acts as a deadline for
    the entire request, not just to connect. Since we return the still-
    open http.Response from the proxy method, we want to leave it open
    indefinitely from that point on.
    
    In this way, it was also inadvertently working as a server timeout; we
    probably want one of those, but that should be addressed separately
    (and not be exclusive to proxied requests).

--- proxy.go
+++ proxy.go
@@ -18,7 +18,10 @@ type proxyResponse struct {
 	err  error
 }
 
-var errProxyTimeout = errors.New("all peers timed out")
+var (
+	errProxyTimeout    = errors.New("all peers timed out")
+	errRequestCanceled = errors.New("client-side request canceled")
+)
 
 // proxy proxies the request, trying each peer that should have the key
 // in turn. The total logical attempt will be capped at the configured proxy
@@ -38,18 +41,35 @@ var errProxyTimeout = errors.New("all peers timed out")
 //     cancels any running requests.
 func (vs *version) proxy(r *http.Request, peers []string) (*http.Response, string, error) {
 	responses := make(chan proxyResponse, len(peers))
-	ctx, _ := context.WithDeadline(r.Context(),
-		time.Now().Add(vs.sequins.config.Sharding.ProxyTimeout.Duration))
+	totalTimeout := time.NewTimer(vs.sequins.config.Sharding.ProxyTimeout.Duration)
+	ctx, cancel := context.WithCancel(r.Context())
 
-	peerIndex := 0
+	// Not canceling here will leak a reference to this context on the parent
+	// context. However, we don't want to cancel when we return out of the method,
+	// since we want the returned http.Response.Body to be left open for
+	// streaming. Ultimately, it'll get canceled (and GC'd) when the parent
+	// context does anyway - and its parent is the incoming request context, which
+	// means it's canceled almost immediately.
+	// defer cancel()
+
 	outstanding := 0
-	for {
+	cancels := make(map[string]context.CancelFunc, len(peers))
+	for peerIndex := 0; ; peerIndex++ {
 		stageTimeout := time.NewTimer(vs.sequins.config.Sharding.ProxyStageTimeout.Duration)
+
 		if peerIndex < len(peers) {
 			peer := peers[peerIndex]
-			go vs.proxyAttempt(ctx, r.URL.Path, peer, responses)
-			peerIndex += 1
-			outstanding += 1
+
+			attemptCtx, cancelAttempt := context.WithCancel(ctx)
+			req, err := vs.newProxyRequest(attemptCtx, r.URL.Path, peer)
+			if err != nil {
+				cancelAttempt()
+				log.Printf("Error initializing request to peer: %s", err)
+			} else {
+				cancels[peer] = cancelAttempt
+				outstanding += 1
+				go vs.proxyAttempt(req, peer, responses)
+			}
 		} else if outstanding == 0 {
 			return nil, "", errNoAvailablePeers
 		}
@@ -58,13 +78,23 @@ func (vs *version) proxy(r *http.Request, peers []string) (*http.Response, strin
 		case res := <-responses:
 			if res.err != nil {
 				log.Printf("Error proxying request to peer: %s", res.err)
+				cancels[res.peer]()
 				outstanding -= 1
-				continue
 			} else {
+				// Cancel any other outstanding attempts.
+				for peer, cancelAttempt := range cancels {
+					if peer != res.peer {
+						cancelAttempt()
+					}
+				}
+
 				return res.resp, res.peer, nil
 			}
-		case <-ctx.Done():
+		case <-totalTimeout.C:
+			cancel()
 			return nil, "", errProxyTimeout
+		case <-ctx.Done():
+			return nil, "", errRequestCanceled
 		case <-stageTimeout.C:
 		}
 	}
@@ -72,9 +102,25 @@ func (vs *version) proxy(r *http.Request, peers []string) (*http.Response, strin
 	return nil, "", errNoAvailablePeers
 }
 
-func (vs *version) proxyAttempt(ctx context.Context, path, peer string, res chan proxyResponse) {
-	// Create a fresh request, so we don't pass on any baggage like
-	// 'Connection: close' headers.
+func (vs *version) proxyAttempt(proxyRequest *http.Request, peer string, res chan proxyResponse) {
+	resp, err := http.DefaultClient.Do(proxyRequest)
+	if err != nil {
+		res <- proxyResponse{nil, peer, err}
+		return
+	}
+
+	if resp.StatusCode != 200 && resp.StatusCode != 404 {
+		resp.Body.Close()
+		res <- proxyResponse{nil, peer, fmt.Errorf("got %d", resp.StatusCode)}
+		return
+	}
+
+	res <- proxyResponse{resp, peer, nil}
+}
+
+// newProxyRequest creates a fresh request, to avoid passing on baggage like
+// 'Connection: close' headers.
+func (vs *version) newProxyRequest(ctx context.Context, path, peer string) (*http.Request, error) {
 	url := &url.URL{
 		Scheme:   "http",
 		Host:     peer,
@@ -82,23 +128,10 @@ func (vs *version) proxyAttempt(ctx context.Context, path, peer string, res chan
 		RawQuery: fmt.Sprintf("proxy=%s", vs.name),
 	}
 
-	proxyRequest, err := http.NewRequest("GET", url.String(), nil)
+	req, err := http.NewRequest("GET", url.String(), nil)
 	if err != nil {
-		res <- proxyResponse{nil, "", err}
-		return
+		return req, err
 	}
 
-	resp, err := http.DefaultClient.Do(proxyRequest.WithContext(ctx))
-	if err != nil {
-		res <- proxyResponse{nil, "", err}
-		return
-	}
-
-	if resp.StatusCode != 200 && resp.StatusCode != 404 {
-		resp.Body.Close()
-		res <- proxyResponse{nil, "", fmt.Errorf("got %d", resp.StatusCode)}
-		return
-	}
-
-	res <- proxyResponse{resp, peer, nil}
+	return req.WithContext(ctx), nil
 }

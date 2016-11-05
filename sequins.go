351c0589f7db5eb8a95c196034df1125deaca617
Colin Marc <colinmarc@gmail.com>
2016-11-05 15:08:49 +0100

    Add log line when we remove a database

--- sequins.go
+++ sequins.go
@@ -256,6 +256,7 @@ func (s *sequins) refreshAll() {
 
 	for name, db := range oldDBs {
 		if s.dbs[name] == nil {
+			log.Println("Removing and clearing database %s", name)
 			db.close()
 			db.delete()
 		}

2e7d821f3351d8c5f09f17a181d946ca6bebe238
David Judd <djudd@stripe.com>
2017-07-31 17:45:48 -0700

    forgot unzip

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -8,6 +8,7 @@ cd /build/proto
 wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip
 echo '939fece2ccef965864e193abba0650c597f7b0e7bedc618c5f2bf13871f7cbb3a655df25ea72cb8a10d23f698c3bc2b1541b2a29fe8f3b0b98cd47b056e62d0f  protoc-3.3.0-linux-x86_64.zip' > checksum
 sha512sum -c checksum
+unzip protoc-3.3.0-linux-x86_64.zip
 
 # Install proto-gen-go
 mkdir -p /go/src/github.com/golang

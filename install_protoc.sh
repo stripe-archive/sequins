f115773a5f17e55bff81ec05ee5bf2b99ba74844
David Judd <djudd@stripe.com>
2017-08-02 15:04:17 -0700

    cannot assume OSTYPE in sh

--- install_protoc.sh
+++ install_protoc.sh
@@ -8,14 +8,15 @@ INSTALL_ROOT=$(cd $1 && pwd)
 
 # Install protoc
 cd $INSTALL_ROOT
-if [[ $OSTYPE == linux* ]]; then
+UNAME=$(uname)
+if [[ $UNAME == Linux* ]]; then
   wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip
   echo '939fece2ccef965864e193abba0650c597f7b0e7bedc618c5f2bf13871f7cbb3a655df25ea72cb8a10d23f698c3bc2b1541b2a29fe8f3b0b98cd47b056e62d0f  protoc-3.3.0-linux-x86_64.zip' > checksum
-elif [[ $OSTYPE == darwin* ]]; then
+elif [[ $UNAME == Darwin* ]]; then
   wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-osx-x86_64.zip
   echo '4b26dc9a38413de6843379cfe1d6d143dc7b0732d199f5a529120abffa6df912556d184f409d257bef7099828d04c88c405d3dcb1ae1be95358538567bfeb9e7  protoc-3.3.0-osx-x86_64.zip' > checksum
 else
-  echo "Unrecognized OS $OSTYPE"
+  echo "Unrecognized OS $UNAME"
   exit 1
 fi
 
